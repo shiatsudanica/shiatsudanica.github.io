9b91c16cf8053a24bc0c203b361c98cfc1aee1c3
shiatsudanica <fards965@gmail.com>
2026-06-12 11:05:08 +0200

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
 // Pro lokalni server nechte prazdne. Pro GitHub Pages sem dejte URL Cloudflare Workeru.
 // Priklad: window.SHIATSU_API_BASE_URL = "https://shiatsu-api.vase-jmeno.workers.dev";
-window.SHIATSU_API_BASE_URL = "aged-dream-f6d6.fards965.workers.dev";
+window.SHIATSU_API_BASE_URL = "https://aged-dream-f6d6.fards965.workers.dev";
